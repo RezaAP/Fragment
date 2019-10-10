89a5a0c987f30bf13e3235fb01e942e44ec159b0
RezaAP <rezaariestyaputra@gmail.com>
2019-10-10 15:26:19 +0700

    done

---
+++ src/main/java/com/example/fragment/activities/StaticActivity.java
@@ -0,0 +1,17 @@
+package com.example.fragment.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fragment.R;
+
+public class StaticActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_static);
+    }
+
+}
